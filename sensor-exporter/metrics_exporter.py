27cf1ad9e12076dc07bdbc02da6bcbd764a48df1
Uladzislau Vauchok <uvauchok@workfusion.com>
2025-01-26 21:31:23 +0400

    Add metrics exporter v2

--- sensor-exporter/metrics_exporter.py
+++ sensor-exporter/metrics_exporter.py
@@ -2,15 +2,30 @@ from prometheus_client import Gauge
 
 class MetricsExporter:
     def __init__(self):
+        self.temperature_metric = Gauge(
+            "temperature_inside",
+            "Temperature metrics from all sensors",
+            ["sensor"]
+        self.humidity_metric = Gauge(
+            "humidity_inside",
+            "Humidity metrics from all sensors",
+            ["sensor"]
+        )
         self.metrics = {}
 
-    def register_sensor(self, name, metric_type="temperature"):
-        metric_key = f"{metric_type}_{name}"
+    def set_temperature(self, sensor_name, value):
+        self.temperature_metric.labels(sensor=sensor_name).set(value)
+
+    def set_humidity(self, sensor_name, value):
+        self.humidity_metric.labels(sensor=sensor_name).set(value)
+
+    def register_sensor(self, sensor_name, metric_type="metric"):
+        metric_key = f"{metric_type}_{sensor_name}"
         if metric_key not in self.metrics:
-            self.metrics[metric_key] = Gauge(metric_key, f"{metric_type.capitalize()} metric for {name}")
+            self.metrics[metric_key] = Gauge(metric_key, f"{metric_type.capitalize()} metric for {sensor_name}")
         return self.metrics[metric_key]
 
-    def update_metric(self, name, metric_type, value):
-        metric_key = f"{metric_type}_{name}"
+    def update_metric(self, sensor_name, metric_type, value):
+        metric_key = f"{metric_type}_{sensor_name}"
         if metric_key in self.metrics:
             self.metrics[metric_key].set(value)
--- sensor-exporter/temperature_sensor.py
+++ sensor-exporter/temperature_sensor.py
@@ -6,8 +6,7 @@ class TemperatureSensorDHT22:
         self.name = name
         self.sensor = adafruit_dht.DHT22(pin)
         self.logger = logging.getLogger(self.__class__.__name__)
-        self.temperature_metric = metrics_exporter.register_sensor(name, "temperature")
-        self.humidity_metric = metrics_exporter.register_sensor(name, "humidity")
+        self.metrics_exporter = metrics_exporter
         self.logger.info("Temperature sensor initialized on pin %s", pin)
 
     def read(self):
@@ -17,19 +16,19 @@ class TemperatureSensorDHT22:
             self.logger.info("%s; Temperature: %s°C, Humidity: %s%%", self.name, temperature, humidity)
 
             if temperature is not None:
-                self.temperature_metric.set(temperature)
+                self.metrics_exporter.set_temperature(self.name, temperature)
             else:
-                self.temperature_metric.set(float('nan'))
+                self.metrics_exporter.set_temperature(self.name, float('nan'))
             if humidity is not None:
-                self.humidity_metric.set(humidity)
+                self.metrics_exporter.set_humidity(self.name, humidity)
             else:
-                self.humidity_metric.set(float('nan'))
+                self.metrics_exporter.set_humidity(self.name, float('nan'))
 
             return temperature, humidity
         except RuntimeError as error:
             self.logger.warning("Error reading from sensor: %s", error)
-            self.temperature_metric.set(float('nan'))
-            self.humidity_metric.set(float('nan'))
+            self.metrics_exporter.set_temperature(self.name, float('nan'))
+            self.metrics_exporter.set_humidity(self.name, float('nan'))
             return None, None
 
     def cleanup(self):
