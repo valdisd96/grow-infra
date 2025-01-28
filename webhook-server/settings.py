72f02c24194af5f8e0d16aebd6c3f96884809be8
Uladzislau Vauchok <uvauchok@workfusion.com>
2025-01-28 19:05:45 +0400

    add light class

--- webhook-server/settings.py
+++ webhook-server/settings.py
@@ -1,4 +1,5 @@
 import os
-RELAY_PIN = int(os.environ.get("RELAY_PIN", 6))
-TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "")
-TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "")
+FUN_RELAY_PIN = int(os.environ.get("FUN_RELAY_PIN", 6))
+LIGHT_RELAY_PIN = int(os.environ.get("LIGHT_RELAY_PIN", 5))
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "7900309601:AAF5H0v6gJVoSqlQnlfik2qSLjAjXLVYrg4")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "1283394836")
