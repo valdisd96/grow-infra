fd47f7eaeef5f8873d64cb801d1ff0103332ff64
Uladzislau Vauchok <uvauchok@workfusion.com>
2025-06-05 01:31:09 +0400

    delete-sens

--- webhook-server/settings.py
+++ webhook-server/settings.py
@@ -1,5 +1,5 @@
 import os
 FUN_RELAY_PIN = int(os.environ.get("FUN_RELAY_PIN", 6))
 LIGHT_RELAY_PIN = int(os.environ.get("LIGHT_RELAY_PIN", 5))
-TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "7900309601:AAF5H0v6gJVoSqlQnlfik2qSLjAjXLVYrg4")
-TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "1283394836")
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "token")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "chat_id")
